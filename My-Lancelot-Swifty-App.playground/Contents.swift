aa5b887b1b5ca736dab3dfb4d3f8cc3ff9610f54
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-04-17 23:39:03 +0100

    finished adventure #1

--- My-Lancelot-Swifty-App.playground/Contents.swift
+++ My-Lancelot-Swifty-App.playground/Contents.swift
@@ -2,8 +2,17 @@
 
 import UIKit
 
+//Adventure #1
 let str1 = "Sir"
 let str2 = "Lancelot"
 let hero = str1 + str2
 
 print(hero)
+
+//Adventrue #2
+var lvl = 1
+lvl += 1
+let txt = "Sir L. reached level"
+print(txt, terminator:" ")
+print(lvl, terminator: "!")
+
